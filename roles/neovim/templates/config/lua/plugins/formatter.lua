af56518193bb7f6b8d0f0f237737b887651d839b
kenchandev <kenchandev@gmail.com>
2025-12-05 17:33:48 -0500

    chore(neovim): refactored formatter to use built-in methods, added biome and ruff

--- roles/neovim/templates/config/lua/plugins/formatter.lua
+++ roles/neovim/templates/config/lua/plugins/formatter.lua
@@ -1,84 +1,67 @@
 local safe_require = require("utils").safe_require
 
-local util = safe_require("formatter.util")
 local formatter = safe_require("formatter")
-local formatter_filetypes_any = safe_require("formatter.filetypes.any")
-local formatter_filetypes_lua = safe_require("formatter.filetypes.lua")
-local formatter_filetypes_python = safe_require("formatter.filetypes.python")
-local formatter_filetypes_rust = safe_require("formatter.filetypes.rust")
+local defaults = safe_require("formatter.defaults")
+local any = safe_require("formatter.filetypes.any")
+local lua = safe_require("formatter.filetypes.lua")
+local python = safe_require("formatter.filetypes.python")
+local rust = safe_require("formatter.filetypes.rust")
 
-if util and formatter then
-  local eslint_d = function()
-    return {
-      exe = "eslint_d",
-      try_node_modules = true,
-      cwd = util.get_current_buffer_file_dir(),
-      args = {
-        "--stdin",
-        "--stdin-filename",
-        util.escape_path(util.get_current_buffer_file_path()),
-        "--fix-to-stdout"
-      },
-      stdin = true
-    }
-  end
-
-  local prettier = function()
-    return {
-      exe = "prettier",
-      try_node_modules = true,
-      args = {
-        "--stdin-filepath",
-        util.escape_path(util.get_current_buffer_file_path())
-      },
-      cwd = util.get_current_buffer_file_dir(),
-      stdin = true
-    }
-  end
-
+if formatter then
   formatter.setup({
     filetype = {
       astro = {
-        prettier
+        defaults.eslint_d,
+        defaults.prettier
       },
       javascript = {
-        eslint_d,
-        prettier
+        defaults.biome,
+        defaults.eslint_d,
+        defaults.prettier
       },
       javascriptreact = {
-        eslint_d,
-        prettier
+        defaults.biome,
+        defaults.eslint_d,
+        defaults.prettier
       },
       json = {
-        prettier
+        defaults.biome,
+        defaults.prettier
+      },
+      jsonc = {
+        defaults.biome,
+        defaults.prettier
       },
       lua = {
-        formatter_filetypes_lua.stylua
+        lua.stylua
       },
       python = {
-        formatter_filetypes_python.black
+        python.black,
+        python.ruff
       },
       rust = {
-        formatter_filetypes_rust.rustfmt
+        rust.rustfmt
       },
       svelte = {
-        eslint_d,
-        prettier
+        defaults.eslint_d,
+        defaults.prettier
       },
       typescript = {
-        eslint_d,
-        prettier
+        defaults.biome,
+        defaults.eslint_d,
+        defaults.prettier
       },
       typescriptreact = {
-        eslint_d,
-        prettier
+        defaults.biome,
+        defaults.eslint_d,
+        defaults.prettier
       },
       yaml = {
-        prettier
+        defaults.prettier
       },
       -- Use the special "*" file type for defining formatter configurations on any file type.
       ["*"] = {
-        formatter_filetypes_any.remove_trailing_whitespace
+        any.remove_trailing_whitespace
       },
     }
   })
